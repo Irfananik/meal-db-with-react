92f78f4f30b0c3d573c79e20f1414ebc0c18dfb2
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-03-24 16:17:07 +0600

    meals property destructuring done

--- src/components/SingleMeal/SingleMeal.js
+++ src/components/SingleMeal/SingleMeal.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './SingleMeal.css'
 
 const SingleMeal = (props) => {
+    const {meal,image,category,instructions} = props
     return (
         <div className="Single-meal-body">
-            <h3>Food name: {props.meal}</h3>
-            <img className="meal-img" src= {props.image} alt="" />
-            <p>Catagory: {props.category}</p>
-            <p>instructions: {props.instructions.slice(0,100)} ...</p>
+            <h3>Food name: {meal}</h3>
+            <img className="meal-img" src= {image} alt="" />
+            <p>Catagory: {category}</p>
+            <p>instructions: {instructions.slice(0,100)} ...</p>
+            <button>
+                Add to Items
+            </button>
         </div>
     );
 };
