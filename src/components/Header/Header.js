5c8fd1c6e67c59e4bdd93a046d891ced0dde5edb
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-03-24 13:53:45 +0600

    project setup and header component done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header-container">
+            <div>
+                <h2>Meal db.com</h2>
+            </div>
+            <div>
+                <a href="/home">Home</a>
+                <a href="/additems">Add Irems</a>
+                <a href="/constact">Contact</a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
